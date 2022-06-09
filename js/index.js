0ffc7c9bab855316441c8d4a166c8f028a0a8d98
Osmin Mendoza <osmin.mendozasevilla@gmail.com>
2022-06-08 21:28:48 -0600

    Actualizacion 08/06/2022

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
-function mostrarModal(){
-    const modal = new bootstrap.Modal(document.getElementById('modal'));
+/*function mostrarModal(){
+    const modal = new bootstrap.Modal(document.getElementById('#modal'));
     modal.show();
-}
+}*/
+
+const modal = document.getElementById('modal')
+const input = document.getElementById('input')
+
+modal.addEventListener('shown.bs.modal', () => {
+  input.focus()
+})
